713c11de62c38b84c431f78efaf8be1d1fd56232
Brandon Sandoval <colima_97@hotmail.com>
2019-12-17 01:45:24 -0600

    First commit

---
+++ Mr_Roboto.ino
@@ -0,0 +1,16 @@
+const int dirpin2 = 13;
+const int steppin2 = 12;
+const int dirpin = 11;
+const int steppin = 10;
+const int Echo = 9;
+const int Trigger = 8;
+const int tiempo = 700;
+const int led_azul = 3;
+
+void setup(){
+
+}
+
+void loop(){
+
+}
